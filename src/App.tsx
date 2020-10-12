6b6c18ca975c488907b437ec360778cafdd8688c
melquiadesmario <melquiadesm@gmail.com>
2020-10-12 14:21:14 -0300

    Stylize Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 
 import './styles/global.css';
-import './styles/pages/landing.css'
+import './styles/pages/landing.css';
+
+import logoImg from './assets/logo.svg';
 
 function App() {
   return (
     <div id='page-landing'>
-      <h1>Hello Dev</h1>
+      <div className='content-wrapper'>
+        <img src={ logoImg } alt='Happy' />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className='location'>
+          <strong>São Caitano</strong>
+          <span>Pernambuco</span>
+        </div>
+
+        <a href='' className='enter-app'>
+          <FiArrowRight size={ 26 } color='rgba(0, 0, 0, 0.6)' />
+        </a>
+      </div>
     </div>
   );
 }
