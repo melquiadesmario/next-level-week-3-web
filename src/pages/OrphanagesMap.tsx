8b4e618da266e6c837b25f575d3c2d911078577f
melquiadesmario <melquiadesm@gmail.com>
2020-10-12 17:17:22 -0300

    Add Leatlet to working maps

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css';
 
@@ -15,14 +18,23 @@ function OrphanagesMap(){
                     <h2>Escolha um orfanato no mapa</h2>
                     <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
-
+                
                 <footer>
                     <strong>São Caetano</strong>
                     <span>Pernambuco</span>
                 </footer>
             </aside>
             
-            <div></div>
+            <Map
+                center={[-8.286029,-35.9689911]}
+                zoom={ 15 }
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/*<TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />*/}
+                <TileLayer
+                    url={ `https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.REACT_APP_MAPBOX_TOKEN }` }
+                />
+            </Map>
 
             <Link to='' className='create-orphanage'>
                 <FiPlus size={ 32 } color='#FFF' />
