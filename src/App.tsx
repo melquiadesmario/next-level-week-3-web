2f9f57761dc54cf7ecce997feaa09154d59e402f
melquiadesmario <melquiadesm@gmail.com>
2020-10-12 12:35:21 -0300

    Add Assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css'
+
 function App() {
   return (
-    <div>
+    <div id='page-landing'>
       <h1>Hello Dev</h1>
     </div>
   );
